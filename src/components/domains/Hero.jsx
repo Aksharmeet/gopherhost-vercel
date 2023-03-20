6e26773d24df82985eb1e5b6a427c5c8842dace3
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-03-20 09:40:28 +0530

    feat: general-hero-changes

--- src/components/domains/Hero.jsx
+++ src/components/domains/Hero.jsx
@@ -6,6 +6,31 @@ import Cloud from '@/images/cloud.jpg';
 function Hero() {
   return (
     <section className=' mx-auto w-[100vw] bg-gradient-to-r from-[#3ea1db] to-[#0164FF] py-12  text-center   lg:text-left'>
+      <div className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+        <svg
+          className='relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]'
+          viewBox='0 0 1155 678'
+        >
+          <path
+            fill='url(#9b2541ea-d39d-499b-bd42-aeea3e93f5ff)'
+            fillOpacity='.3'
+            d='M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z'
+          />
+          <defs>
+            <linearGradient
+              id='9b2541ea-d39d-499b-bd42-aeea3e93f5ff'
+              x1='1155.49'
+              x2='-78.208'
+              y1='.177'
+              y2='474.645'
+              gradientUnits='userSpaceOnUse'
+            >
+              <stop stopColor='#2563FA ' />
+              <stop offset={1} stopColor='#00E5FF' />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
       <div className='mx-auto flex max-w-screen-xl flex-col gap-10  px-4 py-8  lg:flex-row'>
         <div className='mx-auto max-w-xl place-self-center lg:col-span-7'>
           <h1 className='mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight  text-white md:text-5xl xl:text-6xl'>
--- src/components/hosting/Hero.jsx
+++ src/components/hosting/Hero.jsx
@@ -76,6 +76,31 @@ function BackgroundIllustration(props) {
 export default function Hero() {
   return (
     <div className='lg:pb-30 overflow-hidden py-10 sm:py-10 xl:pb-32'>
+      <div className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+        <svg
+          className='relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]'
+          viewBox='0 0 1155 678'
+        >
+          <path
+            fill='url(#9b2541ea-d39d-499b-bd42-aeea3e93f5ff)'
+            fillOpacity='.3'
+            d='M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z'
+          />
+          <defs>
+            <linearGradient
+              id='9b2541ea-d39d-499b-bd42-aeea3e93f5ff'
+              x1='1155.49'
+              x2='-78.208'
+              y1='.177'
+              y2='474.645'
+              gradientUnits='userSpaceOnUse'
+            >
+              <stop stopColor='#2563FA ' />
+              <stop offset={1} stopColor='#00E5FF' />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
       <Container>
         <div className='lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20'>
           <div className='  relative z-10 mx-auto mt-0 max-w-xl lg:col-span-7 lg:mt-[7rem] lg:max-w-2xl lg:pt-6 xl:col-span-6'>
