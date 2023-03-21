f63c428d29f205c84d1c7b0f1690a1dcc71789e8
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-03-21 19:31:12 +0530

    fix: nav fixed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,9 @@
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Fragment, useEffect, useState } from 'react';
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
   const router = useRouter();
@@ -22,9 +17,9 @@ export default function Navbar() {
     <Disclosure as='nav'>
       {({ open }) => (
         <>
-          <div className='relative z-10  mx-auto max-w-7xl py-5 px-2 sm:px-4 lg:px-8'>
-            <div className='flex h-16 justify-between'>
-              <div className='flex px-2 lg:px-0'>
+          <div className='relative z-10  mx-auto flex max-w-7xl justify-between py-5 px-2 sm:px-4 lg:px-8'>
+            <div className='flex h-16  justify-between'>
+              <div className='flex justify-between px-2 lg:px-0'>
                 <Link
                   href='/'
                   passHref
@@ -45,11 +40,11 @@ export default function Navbar() {
                     height={40}
                   />
                 </Link>
-                <div className='hidden lg:ml-6 lg:flex lg:space-x-8'>
+                <div className=' absolute right-6 top-10 hidden lg:flex lg:space-x-8'>
                   {/* Current: "border-blue-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <Link
                     href='/'
-                    className={`inline-flex items-center border-b-2   px-1 pt-1  text-sm font-medium text-gray-900 hover:border-gray-300  
+                    className={`inline-flex items-center border-b-2   px-1 pt-1  text-sm font-medium  hover:border-gray-300  
                     ${
                       switchColor === '/'
                         ? ' border-blue-500 text-gray-900'
@@ -112,123 +107,36 @@ export default function Navbar() {
                   >
                     Contact
                   </Link>
-                </div>
-              </div>
-              <div className='flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end'>
-                <div className='w-full max-w-lg lg:max-w-xs'>
-                  <label htmlFor='search' className='sr-only'>
-                    Search
-                  </label>
-                  <div className='relative'>
-                    <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
-                      <MagnifyingGlassIcon
-                        className='h-5 w-5 text-gray-400'
-                        aria-hidden='true'
-                      />
-                    </div>
-                    <input
-                      id='search'
-                      name='search'
-                      className='block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
-                      placeholder='Search'
-                      type='search'
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className='flex items-center lg:hidden'>
-                {/* Mobile menu button */}
-                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500'>
-                  <span className='sr-only'>Open main menu</span>
-                  {open ? (
-                    <XMarkIcon
-                      className='absolute top-0 z-10 block h-6 w-6'
-                      aria-hidden='true'
-                    />
-                  ) : (
-                    <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className='hidden lg:ml-4 lg:flex lg:items-center'>
-                <button
-                  type='button'
-                  className='flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
-                >
-                  <span className='sr-only'>View notifications</span>
-                  <BellIcon className='h-6 w-6' aria-hidden='true' />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as='div' className='relative ml-4 flex-shrink-0'>
-                  <div>
-                    <Menu.Button className='flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
-                      <span className='sr-only'>Open user menu</span>
-                      <Image
-                        className='h-8 w-8 rounded-full'
-                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                        alt=''
-                        width={40}
-                        height={40}
-                      />
-                    </Menu.Button>
-                  </div>
-                  {/* dropdown */}
-                  <Transition
-                    as={Fragment}
-                    enter='transition ease-out duration-100'
-                    enterFrom='transform opacity-0 scale-95'
-                    enterTo='transform opacity-100 scale-100'
-                    leave='transition ease-in duration-75'
-                    leaveFrom='transform opacity-100 scale-100'
-                    leaveTo='transform opacity-0 scale-95'
+                  <Link
+                    href='/affiliate'
+                    className={`inline-flex items-center border-b-2   px-1 pt-1  text-sm font-medium  hover:border-gray-300  
+                    ${
+                      switchColor === '/affiliate'
+                        ? ' border-blue-500 text-gray-900'
+                        : 'border-transparent text-gray-500'
+                    }`}
                   >
-                    <Menu.Items className='z-1 absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Your Profile
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Settings
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Sign out
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                    Affiliate
+                  </Link>
+                </div>
               </div>
             </div>
+            {/* Mobile menu button */}
+            <div className='flex items-center lg:hidden'>
+              <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500'>
+                <span className='sr-only'>Open main menu</span>
+                {open ? (
+                  <XMarkIcon
+                    className='absolute top-0 z-10 block h-6 w-6'
+                    aria-hidden='true'
+                  />
+                ) : (
+                  <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
+                )}
+              </Disclosure.Button>
+            </div>
           </div>
+
           {/* Mobile */}
           <Disclosure.Panel className='absolute top-0 z-10 h-[100vh] w-[100%] bg-white lg:hidden'>
             <div className='space-y-1 pt-2 pb-3'>
@@ -305,57 +213,17 @@ export default function Navbar() {
               >
                 Contact
               </Disclosure.Button>
-            </div>
-            <div className='border-t border-gray-200 pt-4 pb-3'>
-              <div className='flex items-center px-4'>
-                <div className='flex-shrink-0'>
-                  <Image
-                    className='h-10 w-10 rounded-full'
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    alt=''
-                    width={40}
-                    height={40}
-                  />
-                </div>
-                <div className='ml-3'>
-                  <div className='text-base font-medium text-gray-800'>
-                    Tom Cook
-                  </div>
-                  <div className='text-sm font-medium text-gray-500'>
-                    tom@example.com
-                  </div>
-                </div>
-                <button
-                  type='button'
-                  className='ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
-                >
-                  <span className='sr-only'>View notifications</span>
-                  <BellIcon className='h-6 w-6' aria-hidden='true' />
-                </button>
-              </div>
-              <div className='mt-3 space-y-1'>
-                <Disclosure.Button
-                  as='a'
-                  href='/your-profile'
-                  className='block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800'
-                >
-                  Your Profile
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as='a'
-                  href='/settings'
-                  className='block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800'
-                >
-                  Settings
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as='a'
-                  href='/sign-out'
-                  className='block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800'
-                >
-                  Sign out
-                </Disclosure.Button>
-              </div>
+              <Disclosure.Button
+                as='a'
+                href='/affiliate'
+                className={`block border-l-4  py-2 pl-3 pr-4 text-base font-medium  hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800  ${
+                  switchColor === '/affiliate'
+                    ? '   border-blue-500 bg-blue-50 text-blue-700'
+                    : 'border-transparent text-gray-600'
+                }`}
+              >
+                Affiliate
+              </Disclosure.Button>
             </div>
           </Disclosure.Panel>
         </>
